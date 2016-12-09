023ab4ae18dbf225b45c9c8c88043b10c1d291e5
ybrikeeg <kirbygee@stanford.edu>
2016-12-08 20:23:42 -0800

    Adding ratings

--- learn.py
+++ learn.py
@@ -1,7 +1,9 @@
 # convert json file into matrix
-import pandas
+# import pandas
 
 import json
+import csv
+
 from pprint import pprint
 
 with open('male.json') as data_file:
@@ -57,7 +59,7 @@ for row in data['males']:
         newPerson[HEIGHT] = str(pos[HEIGHT])
         newPerson[WIDTH] = str(pos[WIDTH])
 
-        name = row['url'][row['url'].rfind('/') + 1 : -4]
+        name = str(row['url'][row['url'].rfind('/') + 1 : -4])
         newPerson[NAME] = name
         if name not in usedNames:
             table.append(newPerson)
@@ -66,9 +68,26 @@ for row in data['males']:
         print 'no face for ' + row['url']
 
 
+ratings = []
+i = 0
+with open('femaleRatings.csv', 'rb') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        if i > 0:
+            ratings.append(row)
+        i += 1
+
+for person in table:
+    name = person[NAME]
+    for rating in ratings:
+        if rating[0].replace(".jpg", "") == name:
+            person['Megs'] = rating[1]
+            person['Helen'] = rating[2]
+            person['Sharon'] = rating[3]
+
+
 df = pandas.DataFrame(table)
 df = df.sort_values(by=NAME, ascending=1)
 print df
 males = df.as_matrix()
 print(males)
-
